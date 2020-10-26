76b7be16d940bd1b028fd96c546724c80ea57e2d
Marco Florian Aigner <m.f.aigner@googlemail.com>
2020-10-26 15:35:04 +0100

    Get Data from Foursquare

--- wanderlust/public/main.js
+++ wanderlust/public/main.js
@@ -17,8 +17,22 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
+const getVenues = async () => {
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20201026`;
 
+  try {
+    const response = await fetch(urlToFetch);
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      console.log(venues);
+      return venues;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const getForecast = () => {
