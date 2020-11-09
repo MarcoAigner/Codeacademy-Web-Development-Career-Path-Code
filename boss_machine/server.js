3b32246b89985a44171d0068e1d611da1d1c9908
Marco Florian Aigner <m.f.aigner@googlemail.com>
2020-11-09 03:58:32 +0100

    Added the initial server logic

--- boss_machine/server.js
+++ boss_machine/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 module.exports = app;
 
@@ -9,17 +10,19 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(express.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
 }
--- boss_machine/server/api.js
+++ boss_machine/server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+apiRouter.get('/', (req, res, next) => {
+    console.log('Entered the qpiRouter');
+    res.send('Entered the apiRouter');
+})
 
 
 module.exports = apiRouter;
